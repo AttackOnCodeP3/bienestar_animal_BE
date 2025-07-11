87ce599646b915128ec82a77673e315e090c234b
Adrian Blanco Cordero <63083462+Ablanco1547@users.noreply.github.com>
2025-07-11 04:57:12 -0400

    Refactor forgot password to use DTO and update port
    Changed the forgot password endpoint to accept a ForgotPasswordDTO via @RequestBody instead of a request parameter. Added the ForgotPasswordDTO class. Updated MariaDB port mapping in docker-compose.yml from 3306:3306 to 3307:3306.

--- src/main/java/com/project/demo/rest/user/ForgotPasswordController.java
+++ src/main/java/com/project/demo/rest/user/ForgotPasswordController.java
@@ -1,6 +1,7 @@
 package com.project.demo.rest.user;
 
 import com.project.demo.logic.entity.user.ForgotPasswordService;
+import com.project.demo.rest.user.dto.ForgotPasswordDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,15 +9,16 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
 public class ForgotPasswordController {
 
     @Autowired
     private ForgotPasswordService forgotPasswordService;
-
+@CrossOrigin
     @PostMapping("/forgot-password")
-    public ResponseEntity<?> forgotPassword(@RequestParam String email) throws IOException {
-        boolean sent = forgotPasswordService.resetPasswordAndSendEmail(email);
+    public ResponseEntity<?> forgotPassword(@RequestBody ForgotPasswordDTO request) throws IOException {
+        boolean sent = forgotPasswordService.resetPasswordAndSendEmail(request.getEmail());
         if (sent) {
             return ResponseEntity.ok("Si existe un usuario con ese correo, se ha enviado una contraseña temporal a su correo.");
         } else {
